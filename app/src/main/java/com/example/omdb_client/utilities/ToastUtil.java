2a71ba01f1bc1e05ba4c537b7b58281ee4ce2789
Muhammad nabeel <muhammadnabeel@live.com>
2020-02-03 15:24:35 +0500

    util added and conventions completed

--- app/src/main/java/com/example/omdb_client/utilities/ToastUtil.java
+++ app/src/main/java/com/example/omdb_client/utilities/ToastUtil.java
@@ -1,5 +1,15 @@
 package com.example.omdb_client.utilities;
 
-public class ToastUtil {
+import android.content.Context;
+import android.widget.Toast;
 
+public final class ToastUtil {
+
+    public static void toastLong(Context context, String text) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+    }
+
+    public static void toastShort(Context context, String text) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+    }
 }
